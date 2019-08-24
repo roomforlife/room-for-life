c4b9a113e1df288e4ff1e9bdcde529b4f55ff227
Pawel Maricnkowski <wafel34@gmail.com>
2019-08-24 12:19:10 +0200

    fix about

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -6,7 +6,7 @@ const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
     title={entry.getIn(['data', 'title'])}
     content={widgetFor('body')}
-    heroImage={widgetFor('image')}
+    heroImage={entry.getIn(['data', 'heroImage'])}
   />
 )
 
